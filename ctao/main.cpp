3c02e9710da3690da62b93d960363f5c151df03a
DanieleGravina <daniele.gravina@mail.polimi.it>
2014-09-21 16:02:12 +0200

    Added source file opening, IF stat doesn't work

--- ctao/main.cpp
+++ ctao/main.cpp
@@ -1,11 +1,12 @@
 /* 
- * File:   main.cpp
- * Author: daniele
- *
- * Created on 28 aprile 2014, 11.11
- */
+* File:   main.cpp
+* Author: daniele
+*
+* Created on 28 aprile 2014, 11.11
+*/
 
 #include <string>
+#include <iostream>
 #include <cstdio>
 #include <fstream>
 #include "lexer.h"
@@ -15,48 +16,65 @@
 
 int main(int argc, char **argv) {
 
-    using namespace std;
+	using namespace std;
 
-    /*if (argc != ARGS + 1) {
- fprintf(stderr, "Must use %d arguments", ARGS);
- fputs("\n", stderr);
- exit(1);
-}
+	/*if (argc != ARGS + 1) {
+	fprintf(stderr, "Must use %d arguments", ARGS);
+	fputs("\n", stderr);
+	exit(1);
+	}
 
-    ifstream in(argv[1]);
+	ifstream in(argv[1]);
 
-    if(!in) {
-            fprintf(stderr, "Could not open file %s\n", argv[1]);
-            exit(1);
-    }*/
+	if(!in) {
+	fprintf(stderr, "Could not open file %s\n", argv[1]);
+	exit(1);
+	}*/
 
-    const char* s_punt = 
-		    " VAR x , squ;" 
+	const char* s_punt;
 
-            " PROCEDURE square z ; "
-			" ARRAY y [10]; "
-            " BEGIN "
-			"  squ := z * y[2] ;"
-			"  z := 5 "
-            " END; "
+	string text;
+	stringstream sstr;
+	string path = __FILE__; //gets source code path, include file name
+	path = path.substr(0,1+path.find_last_of('\\')); //removes file name
+	path+= "example.txt"; //adds input file to path
 
-            " BEGIN "
-            " x := 1; "
-            " WHILE x <= 10 DO "
-            "  BEGIN "
-            "  CALL square x ; "
-            "  !squ; "
-            "  x := x + 1 "
-            "  END "
-            " END. ";
+	ifstream myfile (path, ios::in);
+	if (myfile.is_open())
+	{
+		stringstream sstr;
+		sstr << myfile.rdbuf();
+		text = sstr.str();
+		myfile.close();
+	}
+
+	s_punt = text.c_str();
+
+	/*const char* s_punt = 
+		" VAR x , squ;" 
+
+		" PROCEDURE square z ; "
+		" ARRAY y [10]; "
+		" BEGIN "
+		"  squ := z * z ;"
+		"  y[2] := 5 "
+		" END; "
+
+		" BEGIN "
+		" x := 1; "
+		" WHILE x <= 10 DO "
+		"  BEGIN "
+		"  CALL square x ; "
+		"  !squ; "
+		"  x := x + 1 "
+		"  END "
+		" END. ";*/
 
 
-    lexer l(s_punt);
+	lexer l(s_punt);
 
-    frontend f(l);
+	frontend f(l);
 
-    f.program();
-
-    //in.close();
+	f.program();
 }
 
